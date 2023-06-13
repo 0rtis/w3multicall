57e56ed7a78b198d201556a00b329f4640656324
Ortis <ortis@ortis.io>
2023-06-13 15:56:22 +0900

    fix w3pool block

--- src/w3multicall/web3/web3.py
+++ src/w3multicall/web3/web3.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Union, Callable
 import time
 import logging
+import threading
 from web3 import Web3
 
 
@@ -39,6 +40,7 @@ class W3Pool:
         """
         self.w3s = w3s
         self.logger = logger
+        self.lock = threading.RLock()
 
     def add_w3(self, w3: W3) -> 'W3Pool':
         self.w3s.append(w3)
@@ -50,28 +52,34 @@ class W3Pool:
         :param block: (default: true) block until a Web3 instance is available
         :return: Web3 instance
         """
+
         next_available: Union[W3, None] = None
 
-        for i in range(len(self.w3s)):
-            tau = self.w3s[i].usable_in()
-            if tau <= 0:
-                if self.logger is not None:
-                    self.logger.debug("Using {}".format(self.w3s[i]))
-                return self.w3s[i].use()
-            if next_available is None or tau < next_available.usable_in():
-                next_available = self.w3s[i]
+        while next_available is None:
+            sleep = 0
+            with self.lock:
+                for i in range(len(self.w3s)):
+                    tau = self.w3s[i].usable_in()
+                    if tau <= 0:
+                        if self.logger is not None:
+                            self.logger.debug("Using {}".format(self.w3s[i]))
+                        return self.w3s[i].use()
+                    if next_available is None or tau < next_available.usable_in():
+                        next_available = self.w3s[i]
 
-        if next_available is None:
-            raise Exception("No Web3 instance found")
+                if next_available is None:
+                    raise Exception("No Web3 instance found")
+                sleep = next_available.usable_in()
+                if sleep <= 0:
+                    if self.logger is not None:
+                        self.logger.debug("Using {}".format(next_available))
+                    return next_available.use()
 
-        if block:
-            sleep = next_available.usable_in()
-            if sleep > 0:
+            next_available = None
+            if block:
                 if self.logger is not None:
-                    self.logger.warning("Waiting {}s for {}".format(sleep, next_available))
+                    self.logger.warning("Waiting {}s for a slot".format(sleep))
                 time.sleep(sleep)
-            self.logger.debug("Using {}".format(next_available))
-            return next_available.use()
         else:
             return None
 
